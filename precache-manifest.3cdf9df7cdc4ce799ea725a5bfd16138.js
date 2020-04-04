71e38d3c0968577512a05a07d5d6325d9cc89e39
dpkashyap3 <32429850+dpkashyap3@users.noreply.github.com>
2020-04-04 17:40:06 +0530

    Updates

---
+++ precache-manifest.3cdf9df7cdc4ce799ea725a5bfd16138.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d2d246c88a21f4bade48320b3aeb746d",
+    "url": "/receipe/index.html"
+  },
+  {
+    "revision": "4e8a0694ffdd33722773",
+    "url": "/receipe/static/css/2.64a99b10.chunk.css"
+  },
+  {
+    "revision": "4dce4f918b2d7945bce0",
+    "url": "/receipe/static/css/main.1f533711.chunk.css"
+  },
+  {
+    "revision": "4e8a0694ffdd33722773",
+    "url": "/receipe/static/js/2.35ac4d1f.chunk.js"
+  },
+  {
+    "revision": "9f376ff1eaa6c952262f559ceb6fee73",
+    "url": "/receipe/static/js/2.35ac4d1f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "4dce4f918b2d7945bce0",
+    "url": "/receipe/static/js/main.6679f7c3.chunk.js"
+  },
+  {
+    "revision": "bee504b396ef351f4318",
+    "url": "/receipe/static/js/runtime-main.ed3e070a.js"
+  },
+  {
+    "revision": "966ff648832953d97eb545c7f237b16d",
+    "url": "/receipe/static/media/logo.966ff648.png"
+  }
+]);
